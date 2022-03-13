f3beeb2c915b610a131480dc01466124e8a1fc95
Amir Shariat <amirshariat1987@gmail.com>
2022-03-13 16:33:01 -0600

    connected to the database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +9,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+// Connect to database.
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username.
+    user: 'root',
+    // Your MySQL password.
+    password: 'Amir1987!',
+    database: 'election'
+  },
+  console.log("Connected to the election database!")
+);
+
+// Query the database.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// Default response for any other request (Not Found).
 app.use((req, res) => {
   res.status(404).end();
 });
